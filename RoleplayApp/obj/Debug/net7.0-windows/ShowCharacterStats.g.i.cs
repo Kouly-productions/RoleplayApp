f607c13c9ba629b12dc6d4e28987e78bbdaa7cc3
DayKnight <elikouly@gmail.com>
2023-10-20 07:43:33 +0200

    commit

--- RoleplayApp/obj/Debug/net7.0-windows/ShowCharacterStats.g.i.cs
+++ RoleplayApp/obj/Debug/net7.0-windows/ShowCharacterStats.g.i.cs
@@ -237,7 +237,7 @@ namespace RoleplayApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RoleplayApp;component/showcharacterstats.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RoleplayApp;V1.0.0.0;component/showcharacterstats.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ShowCharacterStats.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
